aff63116e77e8e1733a30d9e23508ee0c1c5dc3f
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-10-02 14:01:48 +0100

    PP-5670: Specify the consumer when running contract test
    Currently when a consumer build is kicked off, the provider contract test will
    run in its entirety. For example, in a selfservice build, the adminusers provider
    contract tests will run as well. However, what is run is:
    
    products-ui->adminusers
    selfservice->adminusers
    
    We really only want to run selfservice->adminusers in this case.
    
    This commit will allow a dynamic creation of the test suite to run based on a
    CONSUMER system property. This will mean shorter run times when a provider
    contract test is run as part of a specific consumer build.

--- src/test/java/uk/gov/pay/adminusers/pact/ContractTestSuite.java
+++ src/test/java/uk/gov/pay/adminusers/pact/ContractTestSuite.java
@@ -1,13 +1,40 @@
 package uk.gov.pay.adminusers.pact;
 
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
 import org.junit.runners.Suite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@RunWith(Suite.class)
+import java.util.Map;
 
-@Suite.SuiteClasses({
-        ProviderContractTest.class
-})
+import static java.lang.String.format;
+
+@RunWith(AllTests.class)
 public class ContractTestSuite {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContractTestSuite.class);
 
+    private static Map<String, JUnit4TestAdapter> map = Map.of(
+            "products-ui", new JUnit4TestAdapter(ProductsUIContractTest.class),
+            "selfservice", new JUnit4TestAdapter(SelfServiceContractTest.class));
+
+    public static TestSuite suite() {
+        String consumer = System.getProperty("CONSUMER");
+        TestSuite suite = new TestSuite();
+
+        if (consumer == null || consumer.isBlank()) {
+            LOGGER.info("Running all contract tests.");
+            map.forEach((key, value) -> suite.addTest(value));
+        } else if (map.containsKey(consumer)) {
+            LOGGER.info("Running {}-connector contract tests only.", consumer);
+            suite.addTest(map.get(consumer));
+        } else {
+            throw new RuntimeException(format("Error running provider contract tests. ${CONSUMER} system property was %s.", consumer));
+        }
+
+        return suite;
+    }
 }
